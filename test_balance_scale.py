bf312030a5e2a980f678460fc5f45418e7d05eb4
Anders Friis <Friis73071@gmail.com>
2016-10-31 09:46:27 -0400

    first commit

---
+++ test_balance_scale.py
@@ -0,0 +1,47 @@
+import unittest
+
+class BalanceScaleTestCase(unittest.TestCase):
+    def SetUp(self):
+        self.scale = BalanceScale()
+    
+
+
+    def test_compare_return_negative_one_when_left_weighs_more_than_right(self):
+        left = [1 ,1 2]
+        right = [1, 1 1]
+        
+        self.assertEqual(self.scale.compare(left, right), -1)
+
+    def test_compare_return_zero_when_left_equals_right(self):
+        left = [1 ,1  1]
+        right = [1, 1 1]
+        
+        self.assertEqual(self.scale.compare(left, right), 0)
+
+    def test_compare_return_one_when_left_less_ than_right(self):
+        left = [1 ,1  1]
+        right = [1, 1 1]
+        
+        self.assertEqual(self.scale.compare(left, right), 1)
+    def test_compare_raises_ValueError_if_len_left_not_equal_len_right(self):
+        left = [1]
+        right = [1, 2, 3]
+
+        self.assertRaises(ValueError, self.scale.compare, left, right)
+    def test_compare_raises_BalanceUsedTooManyTimesError_if_used_more_than_3_times(self):
+        self.scale.compare ([0], [0])
+        self.scale.compare ([0], [0])
+        self.scale.compare ([0], [0])
+
+        self.assertRaises(BalanceUsedTooManyTimesError, self.scale.compare, [0], [0]
+
+
+
+
+
+    
+
+
+
+
+
